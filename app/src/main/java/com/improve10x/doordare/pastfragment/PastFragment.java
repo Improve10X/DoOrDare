39042d29205e5a477d4a9d359ba31119bc20d559
Omprakash <omprakash.mopuri@improve10x.com>
2022-12-31 12:00:16 +0530

    Implement the progress bar in PastFragment

--- app/src/main/java/com/improve10x/doordare/pastfragment/PastFragment.java
+++ app/src/main/java/com/improve10x/doordare/pastfragment/PastFragment.java
@@ -51,6 +51,7 @@ public class PastFragment extends Fragment {
     }
 
     private void fetchData() {
+        showProgressBar();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("tasks")
                 .get()
@@ -58,10 +59,19 @@ public class PastFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull com.google.android.gms.tasks.Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
+                            hideProgressBar();
                             List<Task> tasks = task.getResult().toObjects(Task.class);
                             pastTasksAdapter.setData(tasks);
                         }
                     }
                 });
+    }
+
+    private void showProgressBar() {
+        binding.progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        binding.progressBar.setVisibility(View.GONE);
     }
 }
