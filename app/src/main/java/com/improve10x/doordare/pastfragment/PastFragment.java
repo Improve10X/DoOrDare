26f776a140d55d7a37a7d54d052b02b67980f643
Omprakash <omprakash.mopuri@improve10x.com>
2022-12-31 10:47:51 +0530

    Implement the get functionality in PastFragment

--- app/src/main/java/com/improve10x/doordare/pastfragment/PastFragment.java
+++ app/src/main/java/com/improve10x/doordare/pastfragment/PastFragment.java
@@ -2,6 +2,7 @@ package com.improve10x.doordare.pastfragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -9,43 +10,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.improve10x.doordare.Task;
 import com.improve10x.doordare.databinding.FragmentPastBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PastFragment extends Fragment {
 
     private FragmentPastBinding binding;
-    private ArrayList<PastTask> pastTasks;
+    private ArrayList<Task> tasks = new ArrayList<>();
     private PastTasksAdapter pastTasksAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPastBinding.inflate(getLayoutInflater());
-        setData();
         setupPastTasksAdapter();
         setupPastTasksRv();
         return binding.getRoot();
     }
 
-    private void setData() {
-        pastTasks = new ArrayList<>();
-        PastTask pastTask = new PastTask("2", "Do : some task", "Dare : some dare", "10Am", "29", "dec, 2022", "1 day left");
-        pastTasks.add(pastTask);
-        pastTasks.add(pastTask);
-        pastTasks.add(pastTask);
-        pastTasks.add(pastTask);
-        pastTasks.add(pastTask);
+    @Override
+    public void onResume() {
+        super.onResume();
+        fetchData();
     }
 
     private void setupPastTasksAdapter() {
         pastTasksAdapter = new PastTasksAdapter();
-        pastTasksAdapter.setData(pastTasks);
+        pastTasksAdapter.setData(tasks);
     }
 
     private void setupPastTasksRv() {
         binding.pastTasksRv.setLayoutManager(new LinearLayoutManager(getActivity()));
         binding.pastTasksRv.setAdapter(pastTasksAdapter);
+    }
+
+    private void fetchData() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("tasks")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Task> tasks = task.getResult().toObjects(Task.class);
+                            pastTasksAdapter.setData(tasks);
+                        }
+                    }
+                });
     }
 }
