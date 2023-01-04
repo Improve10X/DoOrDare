8f7a4b715676a94ffdc6d5bc816119779b9fdfda
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-01-04 23:58:17 +0530

    modify SplashActivity

--- app/src/main/java/com/improve10x/doordare/SplashActivity.java
+++ app/src/main/java/com/improve10x/doordare/SplashActivity.java
@@ -31,7 +31,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             } else {
-                Intent intent = new Intent(this, LoginActivity.class);
+                Intent intent = new Intent(this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
