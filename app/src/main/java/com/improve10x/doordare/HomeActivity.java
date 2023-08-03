533260758519273a7d6721fbccfdd842473dc6cb
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-08-03 18:09:59 +0530

    implemented login with mobile functionality in HomeActivity

--- app/src/main/java/com/improve10x/doordare/HomeActivity.java
+++ app/src/main/java/com/improve10x/doordare/HomeActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.viewpager.widget.ViewPager;
@@ -17,21 +22,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.improve10x.doordare.base.BaseActivity;
 import com.improve10x.doordare.ui.main.SectionsPagerAdapter;
 import com.improve10x.doordare.databinding.ActivityHomeBinding;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeActivity extends BaseActivity {
 
     private ActivityHomeBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Do / Dare");
-
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -51,6 +65,7 @@ public class HomeActivity extends AppCompatActivity {
             showSignOutDialog();
             return true;
         } else if (item.getItemId() == R.id.login_with_mobile_number) {
+            convertGuestToMobileNumberLogin();
             Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
             return true;
         } else {
@@ -83,5 +98,40 @@ public class HomeActivity extends AppCompatActivity {
         });
         AlertDialog alertDialog1 = alertDialog.create();
         alertDialog1.show();
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        FirebaseAuth.getInstance().getCurrentUser().linkWithCredential(response.getCredentialForLinking())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(HomeActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        }
+
+    private void convertGuestToMobileNumberLogin() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build());
+        setContentView(R.layout.activity_login);
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.Theme_DoOrDare)
+                .build();
+        signInLauncher.launch(signInIntent);
+       /* AuthCredential credential = PhoneAuthProvider.getCredential();*/
+
     }
 }
