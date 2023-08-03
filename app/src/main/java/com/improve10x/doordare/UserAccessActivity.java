e54f75de062abce741b4f89d97c7ebed44cf1cfa
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-03 14:32:14 +0530

    I implemented Guest Login Functionality.

--- app/src/main/java/com/improve10x/doordare/UserAccessActivity.java
+++ app/src/main/java/com/improve10x/doordare/UserAccessActivity.java
@@ -5,23 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.improve10x.doordare.base.BaseActivity;
 import com.improve10x.doordare.databinding.ActivityUserAccessBinding;
 
-public class UserAccessActivity extends AppCompatActivity {
+public class UserAccessActivity extends BaseActivity {
 
     private ActivityUserAccessBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserAccessBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        firebaseAuth = FirebaseAuth.getInstance();
         handleLoginBtn();
         handleGuestBtn();
     }
 
     private void handleGuestBtn() {
-
+        binding.guestBtn.setOnClickListener(v -> {
+            firebaseAuth.signInAnonymously()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        showToast("Guest Login Failed");
+                    });
+        });
     }
 
     private void handleLoginBtn() {
