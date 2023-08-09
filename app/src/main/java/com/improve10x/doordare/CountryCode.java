8b22a9ce84aaeebc335d95dc477c327479ddd5e1
Manohar <manohar.mangali@improve10x.com>
2023-08-09 17:44:05 +0530

    I implemented country code class and getCountryCodes method in ConnectMobileNumberDialog

---
+++ app/src/main/java/com/improve10x/doordare/CountryCode.java
@@ -0,0 +1,14 @@
+package com.improve10x.doordare;
+
+import java.util.ArrayList;
+
+public class CountryCode {
+
+    private String countryName;
+    private String countryCode;
+
+    public CountryCode(String countryName, String countryCode) {
+        this.countryName = countryName;
+        this.countryCode = countryCode;
+    }
+}
